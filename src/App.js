839d2f6db2757414176091757736f465a91d83cc
aaryak4554 <joinaarya30@gmail.com>
2023-11-16 20:04:27 +0530

    codeforces card added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Leet from './leetcode/Leetcode';
+import Codeforces from './codeforces/cf.jsx'
 import GetData from './leetcode/api/api';
+
 function App() {
     return (
         <div>
             <Leet/>
             <GetData/>
+            <Codeforces/>
         </div>
     );
 }
